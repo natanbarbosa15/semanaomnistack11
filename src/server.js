d48a93a9a31782d8068d2ddad06dca610a71acab
Natan Barbosa <natanbarbosa15@gmail.com>
2020-06-09 19:04:25 -0300

    Google Cloud Trace and Debug libs.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("@google-cloud/trace-agent").start();
+require("@google-cloud/debug-agent").start({ allowExpressions: true });
+
 const app = require("./app");
 
 app.listen(process.env.PORT);
