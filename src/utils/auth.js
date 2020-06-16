eefc3513ec758b6bfd8dee7f1a053f55feef035c
Natan Barbosa <natanbarbosa15@gmail.com>
2020-06-15 21:20:54 -0300

    Authorization Header fix.

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,22 +1,17 @@
 const firebaseAdmin = require("../utils/firebaseAdmin");
-const jwt_decode = require("jwt-decode");
 
 module.exports.getCurrentUser = async (request) => {
   try {
     const encodedHeader = request.header("x-endpoint-api-userinfo");
     if (!encodedHeader) return null;
-    var decodedHeader = undefined;
-    if (process.env.NODE_ENV === "test") {
-      decodedHeader = JSON.parse(Buffer.from(encodedHeader, "base64"));
-    } else {
-      decodedHeader = jwt_decode(encodedHeader);
-    }
-    var user = decodedHeader;
+    var user = undefined;
     if (process.env.NODE_ENV === "development") {
       user = await firebaseAdmin.auth().verifyIdToken(encodedHeader, true);
       user.id = user.uid;
     } else {
+      const decodedHeader = JSON.parse(Buffer.from(encodedHeader, "base64"));
       user = decodedHeader;
+      if (process.env.NODE_ENV === "production") user.id = user.user_id;
     }
     return user;
   } catch (error) {
