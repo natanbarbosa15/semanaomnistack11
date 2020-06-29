3da495303581e97304a232440e79d22a9176e70b
Natan Barbosa <natanbarbosa15@gmail.com>
2020-06-29 16:11:54 -0300

    csp-html-webpack-plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,40 @@
 const { addBabelPlugin, override } = require("customize-cra");
 const SriPlugin = require("webpack-subresource-integrity");
+const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+
+const cspConfigPolicy = {
+  "default-src": "'none'",
+  "base-uri": "'self'",
+  "object-src": "'none'",
+  "script-src": ["'self'", "https://*.googleapis.com"],
+  "style-src": ["'self'", "'unsafe-inline'", "https://*.googleapis.com"],
+  "img-src": ["'self'", "data:"],
+  "connect-src": [
+    "https://gateway-hio7rdwdxq-uc.a.run.app",
+    "https://*.googleapis.com",
+  ],
+  "font-src": ["'self'", "https://*.gstatic.com"],
+  "frame-src": ["'self'", "https://*.google.com", "https://*.iubenda.com"],
+  "form-action": [
+    "'self'",
+    "https://gateway-hio7rdwdxq-uc.a.run.app",
+    "https://*.googleapis.com",
+  ],
+  "manifest-src": "'self'",
+};
+
+const cspConfigOptions = {
+  enabled: true,
+  hashingMethod: "sha384",
+  hashEnabled: {
+    "script-src": true,
+    "style-src": true,
+  },
+  nonceEnabled: {
+    "script-src": false,
+    "style-src": false,
+  },
+};
 
 function configWebpack(config, env) {
   if (!config.plugins) {
@@ -12,6 +47,9 @@ function configWebpack(config, env) {
       new SriPlugin({
         hashFuncNames: ["sha384"],
       })
+    );
+    config.plugins.push(
+      new cspHtmlWebpackPlugin(cspConfigPolicy, cspConfigOptions)
     );
   }
   return config;
